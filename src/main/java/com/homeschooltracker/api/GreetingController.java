920d94337859a0d0e08bc904004ab230c13bbba8
Alison Major <alison.major@gmail.com>
2020-10-18 14:37:18 -0400

    Enable cross-origin in controller

--- src/main/java/com/homeschooltracker/api/GreetingController.java
+++ src/main/java/com/homeschooltracker/api/GreetingController.java
@@ -1,5 +1,6 @@
 package com.homeschooltracker.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class GreetingController {
     private static final String template = "Huzzah, %s!!!";
     private final AtomicLong counter = new AtomicLong();
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
